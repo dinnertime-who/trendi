2e2661341cffcaab839fc78838899aaa2b3ca667
dinnertime-who <dinnertime.dev@gmail.com>
2025-10-19 04:06:15 +0900

    refactor: format package.json and improve API CORS configuration

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,26 +7,27 @@ const app = new Hono();
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
+console.log("allowedOrigins", allowedOrigins);
+
 app.use(
-  "/*",
-  cors({
-    origin: allowedOrigins,
-    allowHeaders: ["Authorization", "Content-Type"],
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  }),
+	"/*",
+	cors({
+		origin: allowedOrigins,
+		allowHeaders: ["Authorization", "Content-Type"],
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+	}),
 );
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
 
 serve(
-  {
-    fetch: app.fetch,
-    port: parseInt(process.env.PORT || "8000", 10),
-  },
-  (info) => {
-    console.log(`Server is running on http://${info.address}:${info.port}`);
-  },
+	{
+		fetch: app.fetch,
+		port: parseInt(process.env.PORT || "8000", 10),
+	},
+	(info) => {
+		console.log(`Server is running on http://${info.address}:${info.port}`);
+	},
 );
